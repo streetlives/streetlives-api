3ab28fa291ff3798cbba96387279d9ddd046d8df
Rovack <rovack@walla.co.il>
2018-01-28 10:10:16 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+
+const app = express();
+app.server = http.createServer(app);
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello.');
+});
+
+app.server.listen(process.env.PORT, () => {
+	console.log(`Started listening on port ${app.server.address().port}`);
+});
+
+export default app;
