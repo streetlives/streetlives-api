9526fd1cd5f22e710378e4c788c9e5fae6286241
Rovack <rovack@walla.co.il>
2018-02-16 10:46:10 -0500

    feature: write first version of first controller
    - Implement basic version of location suggestion route.
    - Add utils for the geometry type, handling points etc.

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -1,10 +1,27 @@
+import models from '../models';
+import geometry from '../utils/geometry';
+
 export default {
   find: (req, res) => {
     res.sendStatus(200);
   },
 
   suggestNew: (req, res) => {
-    res.sendStatus(201);
+    const {
+      name,
+      latitude,
+      longitude,
+      taxonomyIds,
+    } = req.body;
+
+    models.LocationSuggestion.create({
+      name,
+      position: geometry.createPoint(longitude, latitude),
+      taxonomy_ids: taxonomyIds,
+    })
+      .then((locations) => {
+        res.sendStatus(201);
+      });
   },
 
   getInfo: (req, res) => {
