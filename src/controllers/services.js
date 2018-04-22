58e7a6782fbcfd7d0e2fad5b06ce15828011d217
Rovack <rovack@walla.co.il>
2018-04-21 22:28:01 -0400

    Feature/edit endpoints (#2)
    * feature: start adding routes for editing data
    
    - Add routes that the Street Team editing forms will need
      to the routes index.
    - Implement the organization and service routes
      (initial implementation, with some remaining TODOs).
    
    * chore: just updating TODOs
    
    * Add cors
    
    * fix: fix setting/updating taxonomy for services
    
    * feature: add the basic location routes
    
    * feature: add the phone editing routes
    
    - Implement the remaining routes for phones, editing and deleting.
    - Change the phone modification routes to be shallow,
      as the location a phone's under doesn't matter once it's created.
    
    * Added models.Organization, models.Phone, models.PhysicalAddress into locations.getInfo operation
    
    * Cherry-pick and merge 6128741cec802ba3af670b05d752d236c9de1593
    
    * feature: add support for address when updating/creating location
    
    * chore: fix tests and lint broken by additions to get-location-info
    
    - Update a snapshot to reflect the addition of associations.
    - Break down a line that was exceeding lint's max length.
    
    * chore: remove TODOs (added to Trello)
    
    * feature: make address format in getting location consistent with updating

---
+++ src/controllers/services.js
@@ -0,0 +1,74 @@
+import Joi from 'joi';
+import serviceSchemas from './validation/services';
+import models from '../models';
+import { NotFoundError } from '../utils/errors';
+
+export default {
+  create: async (req, res, next) => {
+    try {
+      await Joi.validate(req, serviceSchemas.create, { allowUnknown: true });
+
+      const {
+        name,
+        description,
+        url,
+        taxonomyId,
+        locationId,
+      } = req.body;
+
+      const location = await models.Location.findById(locationId);
+      if (!location) {
+        throw new NotFoundError('Location not found');
+      }
+
+      const taxonomy = await models.Taxonomy.findById(taxonomyId);
+      if (!taxonomy) {
+        throw new NotFoundError('Taxonomy not found');
+      }
+
+      const createdService = await location.createService({
+        name,
+        description,
+        url,
+        organization_id: location.organization_id,
+      });
+
+      await createdService.setTaxonomies([taxonomy]);
+
+      res.status(201).send(createdService);
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  update: async (req, res, next) => {
+    try {
+      await Joi.validate(req, serviceSchemas.update, { allowUnknown: true });
+
+      const { serviceId } = req.params;
+
+      const service = await models.Service.findById(serviceId);
+      if (!service) {
+        throw new NotFoundError('Service not found');
+      }
+
+      const { taxonomyId } = req.body;
+      if (taxonomyId) {
+        const taxonomy = await models.Taxonomy.findById(taxonomyId);
+        if (!taxonomy) {
+          throw new NotFoundError('Taxonomy not found');
+        }
+
+        await service.setTaxonomies([taxonomy]);
+      }
+
+      const editableFields = ['name', 'description', 'url'];
+
+      await service.update(req.body, { fields: editableFields });
+
+      res.sendStatus(204);
+    } catch (err) {
+      next(err);
+    }
+  },
+};
---
+++ src/controllers/validation/organizations.js
@@ -0,0 +1,28 @@
+import Joi from 'joi';
+
+export default {
+  find: {
+    query: Joi.object().keys({
+      searchString: Joi.string().allow(''),
+    }),
+  },
+
+  create: {
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      description: Joi.string().required(),
+      url: Joi.string(),
+    }).required(),
+  },
+
+  update: {
+    params: Joi.object().keys({
+      organizationId: Joi.string().guid().required(),
+    }).required(),
+    body: Joi.object().keys({
+      name: Joi.string(),
+      description: Joi.string(),
+      url: Joi.string(),
+    }).required(),
+  },
+};
