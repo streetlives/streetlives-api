9db9a84477ae2ababf06f895728288165fc4b6e0
rovack <rovack@walla.co.il>
2018-04-27 01:26:33 -0400

    feature: add model for Metadata based on HSDS, for storing change history

---
+++ src/models/metadata.js
@@ -0,0 +1,64 @@
+module.exports = (sequelize, DataTypes) => {
+  const Metadata = sequelize.define('Metadata', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    resource_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    resource_table: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    last_action_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    last_action_type: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    field_name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    previous_value: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    replacement_value: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    updated_by: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  }, {
+    tableName: 'metadata',
+    underscored: true,
+    underscoredAll: true,
+  });
+
+  Metadata.associate = (models) => {
+    Object.keys(models).forEach((modelKey) => {
+      const model = models[modelKey];
+
+      if (model !== models.Metadata) {
+        Metadata.belongsTo(model, { foreignKey: 'resource_id', constraints: false });
+        model.hasMany(Metadata, {
+          foreignKey: 'resource_id',
+          constraints: false,
+          scope: {
+            resource_table: model.tableName,
+          },
+        });
+      }
+    });
+  };
+
+  return Metadata;
+};
