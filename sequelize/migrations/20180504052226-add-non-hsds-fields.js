2876ab260f225f78cc908ea9220c8ba629f245e5
rovack <rovack@walla.co.il>
2018-05-04 01:47:55 -0400

    feature: support additional info field for services
    - Add additional_info to services, in the DB and API.
    - Fix type in migration script from STRING to TEXT.
    - Sort of unrelatedly, added some fields to the tests,
      including additional_info, to verify they're returned.

--- sequelize/migrations/20180504052226-add-non-hsds-fields.js
+++ sequelize/migrations/20180504052226-add-non-hsds-fields.js
@@ -1,7 +1,11 @@
 export default {
-  up: (queryInterface, Sequelize) =>
-    queryInterface.addColumn('locations', 'additional_info', Sequelize.STRING),
+  up: (queryInterface, Sequelize) => Promise.all([
+    queryInterface.addColumn('locations', 'additional_info', Sequelize.TEXT),
+    queryInterface.addColumn('services', 'additional_info', Sequelize.TEXT),
+  ]),
 
-  down: (queryInterface, Sequelize) =>
+  down: (queryInterface, Sequelize) => Promise.all([
     queryInterface.removeColumn('locations', 'additional_info'),
+    queryInterface.removeColumn('services', 'additional_info'),
+  ]),
 };
