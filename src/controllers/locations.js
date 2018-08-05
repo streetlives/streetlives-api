05ad68e4ed643cf2d4e6c3511d485ecf8b2cca73
rovack <rovack@walla.co.il>
2018-08-05 00:45:00 -0400

    feature: improve API for fetching comments
    - Sort the results of the GET /comments endpoint.
    - Filter out info about who posted a comment so it's not returned,
      to better protect privacy (FE not showing it is insufficient).
    - Removed comments from the results of getting location data,
      to avoid issues (e.g. that endpoint would never support pagination).
    - Make the endpoint return empty result when location doesn't exist,
      instead of semantically incorrect 404 (location isn't the resource).

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -54,7 +54,6 @@ export default {
                 models.DocumentsInfo,
               ],
             },
-            models.Comment,
             models.Organization,
             models.Phone,
             models.PhysicalAddress,
