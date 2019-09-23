1b1777d83e8db5b7546138f022531170f61bcb56
rovack <rovack@walla.co.il>
2019-09-23 13:58:36 -0400

    feature: return accessibility with location info

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -115,6 +115,7 @@ export default {
             },
             models.Phone,
             models.PhysicalAddress,
+            models.AccessibilityForDisabilities,
           ],
         },
       );
