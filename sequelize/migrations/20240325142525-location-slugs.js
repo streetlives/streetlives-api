05b367b14d778b982a09e8e0eba69ce0a0dfbb75
Jacob Beard <jake@minnow.io>
2024-03-28 01:07:00 -0600

    Fix lint

--- sequelize/migrations/20240325142525-location-slugs.js
+++ sequelize/migrations/20240325142525-location-slugs.js
@@ -1,73 +1,64 @@
-'use strict';
-const nyc_neighborhoods = require("./Information Architecture - YourPeer - June '23 - Unduplicated zips.json")
 
+const nycNeighborhoods = require("./Information Architecture - YourPeer - June '23 - Unduplicated zips.json"); // eslint-disable-line max-len
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.sequelize.transaction(t => {
-      return Promise.all([
-        queryInterface.createTable('location_slugs', {
-          slug: { 
-            type: Sequelize.DataTypes.STRING,
-            primaryKey: true
+  async up(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(t => Promise.all([
+      queryInterface.createTable('location_slugs', {
+        slug: {
+          type: Sequelize.DataTypes.STRING,
+          primaryKey: true,
+        },
+        location_id: {
+          type: Sequelize.DataTypes.UUID,
+          references: {
+            model: {
+              tableName: 'locations',
+            },
+            key: 'id',
           },
-          location_id: {
-            type: Sequelize.DataTypes.UUID,
-            references: {
-              model: {
-                tableName: 'locations',
-              },
-              key: 'id'
-            }
-          }
-        }),
-        queryInterface.createTable('location_slug_redirects', {
-          slug: { 
-            type: Sequelize.DataTypes.STRING,
-            primaryKey: true
+        },
+      }),
+      queryInterface.createTable('location_slug_redirects', {
+        slug: {
+          type: Sequelize.DataTypes.STRING,
+          primaryKey: true,
+        },
+        location_id: {
+          type: Sequelize.DataTypes.UUID,
+          references: {
+            model: {
+              tableName: 'locations',
+            },
+            key: 'id',
           },
-          location_id: {
-            type: Sequelize.DataTypes.UUID,
-            references: {
-              model: {
-                tableName: 'locations',
-              },
-              key: 'id'
-            }
-          }
-        }),
-        queryInterface.createTable('nyc_neighborhoods', {
-          zip_code: {
-            type: Sequelize.DataTypes.STRING,
-            primaryKey: true
+        },
+      }),
+      queryInterface.createTable('nyc_neighborhoods', {
+        zip_code: {
+          type: Sequelize.DataTypes.STRING,
+          primaryKey: true,
+        },
+        neighborhood_name: {
+          type: Sequelize.DataTypes.STRING,
+        },
+
+      }),
+    ]).then(() => Promise.all(Object.entries(nycNeighborhoods)
+      .map(([neighborhoodName, zipCodes]) => Promise.all(zipCodes.map(zipCode =>
+        queryInterface.sequelize.query(
+          'insert into nyc_neighborhoods values ($1, $2)',
+          {
+            bind: [zipCode, neighborhoodName],
+            type: Sequelize.QueryTypes.INSERT,
           },
-          neighborhood_name: { 
-            type: Sequelize.DataTypes.STRING,
-          },
-          
-        })
-      ]).then(() => {
-        return Promise.all(Object.entries(nyc_neighborhoods).
-          map(([neighborhoodName, zipCodes]) => Promise.all(zipCodes.map(zipCode => 
-            queryInterface.sequelize.query(
-                `insert into nyc_neighborhoods values ($1, $2)`,
-                { 
-                  bind: [zipCode, neighborhoodName],
-                  type: Sequelize.QueryTypes.INSERT 
-                },
-            )
-          ))));
-      }).then(() => {
-        return queryInterface.sequelize.query(
-          `insert into location_slug_redirects select slug, location_id from website_data`,
-          { 
-            type: Sequelize.QueryTypes.INSERT 
-          }
-        );
-      }).then(() => {
-        return queryInterface.sequelize.query(
-          `
+        )))))).then(() => queryInterface.sequelize.query(
+      'insert into location_slug_redirects select slug, location_id from website_data',
+      {
+        type: Sequelize.QueryTypes.INSERT,
+      },
+    )).then(() => queryInterface.sequelize.query(`
           create or replace function translate_slug_characters(slug varchar)
              returns varchar
              language plpgsql
@@ -96,11 +87,8 @@ module.exports = {
             );
           end;
           $$;
-          `
-        );
-      }).then(() => {
-        return queryInterface.sequelize.query(
-          `
+          `))
+      .then(() => queryInterface.sequelize.query(`
             create or replace function get_slug(loc_id uuid)
                returns varchar
                language plpgsql
@@ -121,7 +109,8 @@ module.exports = {
 
               -- get the area
               select neighborhood_name into neighborhood 
-              from nyc_neighborhoods nycn inner join physical_addresses pa on nycn.zip_code = pa.postal_code
+              from nyc_neighborhoods nycn 
+                inner join physical_addresses pa on nycn.zip_code = pa.postal_code
               where pa.location_id = loc_id;
 
               if org_name is not null then
@@ -134,7 +123,8 @@ module.exports = {
 
               _slug := translate_slug_characters(_slug);
 
-              select count(1) into location_slug_count from location_slugs where location_slugs.slug = '/locations/' || _slug;
+              select count(1) into location_slug_count 
+                from location_slugs where location_slugs.slug = '/locations/' || _slug;
 
               -- check if the slug exists in the location_slugs table
               if location_slug_count > 0 then
@@ -150,26 +140,22 @@ module.exports = {
               return '/locations/' || _slug; 
             end;
             $$;
-          `
-        );
-      }).then(() => {
+          `))
+      .then(() =>
         // populate the location_slugs table
-        return queryInterface.sequelize.query(
-          `
+        queryInterface.sequelize.query(`
           DO $$DECLARE temprow record;
           BEGIN
             FOR temprow IN
                     SELECT id FROM locations 
                 LOOP
-                    INSERT INTO location_slugs(slug, location_id) VALUES (get_slug(temprow.id), temprow.id);
+                    INSERT INTO location_slugs(slug, location_id) 
+                      VALUES (get_slug(temprow.id), temprow.id);
                     commit;
                 END LOOP;
           END$$;
-          `
-        );
-      }).then(() => {
-        return queryInterface.sequelize.query(
-          `
+          `))
+      .then(() => queryInterface.sequelize.query(`
             create or replace function do_init_slug()
                returns trigger
                language plpgsql
@@ -180,34 +166,38 @@ module.exports = {
               return new;
             end;
             $$;
-          `
-        );
-      }).then(() => {
+          `))
+      .then(() =>
         // populate the location_slugs table
-        return queryInterface.sequelize.query(
-          `
+        queryInterface.sequelize.query(`
           CREATE TRIGGER init_slug 
              AFTER insert
              ON locations
              FOR EACH  ROW
                  EXECUTE PROCEDURE do_init_slug();
-          `
-        );
-      });
-    });
+          `)));
   },
 
-  async down (queryInterface, Sequelize) {
-    return queryInterface.sequelize.transaction(t => {
-      return Promise.all([
-        queryInterface.dropTable('location_slugs', { transaction: t }),
-        queryInterface.dropTable('location_slug_redirects', { transaction: t }),
-        queryInterface.dropTable('nyc_neighborhoods', { transaction: t }),
-        queryInterface.dropFunction('get_slug', [{type: Sequelize.DataTypes.UUID}], { transaction: t }),
-        queryInterface.dropFunction('translate_slug_characters', [{type: 'varchar'}], { transaction: t }),
-        queryInterface.sequelize.query('drop trigger init_slug on locations', { transaction: t }),
-        queryInterface.dropFunction('do_init_slug', [], { transaction: t }),
-      ]);
-    });
-  }
+  async down(queryInterface, Sequelize) {
+    return queryInterface.sequelize.transaction(t => Promise.all([
+      queryInterface.dropTable('location_slugs', { transaction: t }),
+      queryInterface.dropTable('location_slug_redirects', { transaction: t }),
+      queryInterface.dropTable('nyc_neighborhoods', { transaction: t }),
+      queryInterface.dropFunction(
+        'get_slug',
+        [{ type: Sequelize.DataTypes.UUID }],
+        { transaction: t },
+      ),
+      queryInterface.dropFunction(
+        'translate_slug_characters',
+        [{ type: 'varchar' }],
+        { transaction: t },
+      ),
+      queryInterface.sequelize.query(
+        'drop trigger init_slug on locations',
+        { transaction: t },
+      ),
+      queryInterface.dropFunction('do_init_slug', [], { transaction: t }),
+    ]));
+  },
 };
