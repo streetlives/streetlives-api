3826325995d117e2a5d5cb5d991c9deda25dc498
Jacob Beard <jake@minnow.io>
2024-06-05 06:48:00 -0400

    Integrate modified map from @doobneek1

--- sequelize/migrations/20240523021218-initialize-neighborhood-geometries.js
+++ sequelize/migrations/20240523021218-initialize-neighborhood-geometries.js
@@ -1,11 +1,13 @@
+const fs = require('fs');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     // get the geojson
+
     // eslint-disable-next-line max-len,no-undef
-    const response = await fetch('https://data.dathere.com/dataset/acbbee9e-4e37-4439-8e69-ca906f476ae3/resource/d6db2e12-fc58-4e41-bc58-5bdfb5078131/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson');
-    const geojson = await response.json();
+    // lightly modified from: https://data.dathere.com/dataset/acbbee9e-4e37-4439-8e69-ca906f476ae3/resource/d6db2e12-fc58-4e41-bc58-5bdfb5078131/download/d085e2f8d0b54d4590b1e7d1f35594c1pediacitiesnycneighborhoods.geojson
+    const geojson = JSON.parse(fs.readFileSync(`${__dirname}/map.geojson`, 'utf8'));
     // create the table
     await queryInterface.createTable('nyc_neighborhood_geometries', {
       neighborhood: {
