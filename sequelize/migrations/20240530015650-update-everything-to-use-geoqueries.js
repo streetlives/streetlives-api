e31dc7bc01906c4a693d1f0fad83aeb56b5c5912
Jacob Beard <jake@minnow.io>
2024-06-05 23:10:26 -0400

    create a view that queries the geocode information, e.g. neighborhood, district, etc

--- sequelize/migrations/20240530015650-update-everything-to-use-geoqueries.js
+++ sequelize/migrations/20240530015650-update-everything-to-use-geoqueries.js
@@ -17,11 +17,49 @@ module.exports = {
       DROP FUNCTION IF EXISTS handle_set_default_neighborhood_on_physical_address_update_trigger`);
     // drop the neighborhood field
     await queryInterface.removeColumn('physical_addresses', 'neighborhood');
-    // TODO: create a view that queries the neighborhood, district, etc.
+    // create a view that queries the neighborhood, district, etc.
     // primary key based on the location id. so that we can join to this view.
+    await queryInterface.sequelize.query(`
+      create or replace view locations_geocoded_metadata as 
+        select l.id as location_id, 
+          neighborhoods.*, 
+          boroughs.*, 
+          school_districts.school_district_id, 
+          congressional_districts.congressional_district_id, 
+          community_districts.community_district_id 
+        from locations l,
+          lateral (
+                SELECT nng.neighborhood from nyc_neighborhood_geometries nng
+                  where ST_Contains(nng.geometry, ST_SetSRID(l.position, 4326))
+          ) neighborhoods,
+          lateral ( 
+                SELECT nng.borough 
+                   from nyc_neighborhood_geometries  nng
+                   where ST_Contains(nng.geometry, ST_SetSRID(l.position, 4326))
+          ) boroughs,
+          lateral (
+                SELECT  nd.district_id as school_district_id from nyc_districts nd
+                  where 
+                    ST_Contains(nd.geometry, ST_SetSRID(l.position, 4326))
+                    and nd.type = 'school'
+          ) school_districts,
+          lateral (
+                SELECT nd.district_id as congressional_district_id from nyc_districts nd
+                  where ST_Contains(nd.geometry, ST_SetSRID(l.position, 4326))
+                    and nd.type = 'congressional'
+          ) congressional_districts,
+          lateral (
+                SELECT nd.district_id as community_district_id from nyc_districts nd
+                  where ST_Contains(nd.geometry, ST_SetSRID(l.position, 4326))
+                    and nd.type = 'community'
+          ) community_districts
+    `);
   },
 
   async down(queryInterface, Sequelize) {
+    await queryInterface.sequelize.query(`
+      DROP FUNCTION IF EXISTS locations_geocoded_metadata`
+    );
     // create the neighborhood field
     await (!isTesting ?
       queryInterface.addColumn('physical_addresses', 'neighborhood', {
