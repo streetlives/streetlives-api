8488e3bb9c91518f811f08e23da35f3137fce0f4
Jacob Beard <jake@minnow.io>
2024-06-05 22:49:14 -0400

    drop the neighborhood field; drop triggers that update the neighborhood field

--- sequelize/migrations/20240530015650-update-everything-to-use-geoqueries.js
+++ sequelize/migrations/20240530015650-update-everything-to-use-geoqueries.js
@@ -1,120 +1,92 @@
 
+const isTesting = process.env.NODE_ENV === 'test';
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // update the locations.neighborhood database table
-    await queryInterface.sequelize.transaction(async t => Promise.all([
-      queryInterface.addColumn('physical_addresses', 'borough', {
-        type: Sequelize.DataTypes.STRING,
-      }),
-      queryInterface.addColumn('physical_addresses', 'school_district', {
-        type: Sequelize.DataTypes.INTEGER,
-      }),
-      queryInterface.addColumn('physical_addresses', 'congressional_district', {
-        type: Sequelize.DataTypes.INTEGER,
-      }),
-      queryInterface.addColumn('physical_addresses', 'community_district', {
-        type: Sequelize.DataTypes.INTEGER,
-      }),
-    ]));
-
-    // clear existing neighborhoods
+    // delete existing triggers that write to the neighborhood field
     await queryInterface.sequelize.query(`
-      update physical_addresses
-        set neighborhood = null, borough = null;
-    `);
-
-    // modify the trigger function in which the neighborhood
-    // gets derived and set on the physical_addresses table
+      DROP TRIGGER IF EXISTS set_default_neighborhood_on_physical_address_insert_trigger 
+        ON physical_addresses`);
     await queryInterface.sequelize.query(`
-      DO $$
-      DECLARE location_to_update record;
-      BEGIN
-        FOR location_to_update IN
-                select pa.id, nng.neighborhood, nng.borough from
-                physical_addresses pa
-                inner join locations l on pa.location_id = l.id
-                cross join nyc_neighborhood_geometries  nng
-                where ST_Contains(nng.geometry, ST_SetSRID(l.position, 4326))
-            LOOP
-                update physical_addresses set 
-                  neighborhood = location_to_update.neighborhood, 
-                  borough = location_to_update.borough
-                  where physical_addresses.id = location_to_update.id;
-            END LOOP;
-      END$$;
-    `);
-
-    // update the database trigger that keeps this up-to-date
+      DROP FUNCTION IF EXISTS handle_set_default_neighborhood_on_physical_address_insert_trigger`);
     await queryInterface.sequelize.query(`
-      create or replace function 
-        handle_set_default_neighborhood_on_physical_address_insert_trigger()
-         returns trigger
-         language plpgsql
-        as
-      $$
-      DECLARE 
-        default_neighborhood varchar;
-        default_borough varchar;
-        default_school_district int;
-        default_congressional_district int;
-        default_community_district int;
-      begin
-
-        SELECT nng.neighborhood into default_neighborhood from
-          locations l
-          cross join nyc_neighborhood_geometries  nng
-          where l.id = NEW.location_id 
-          and ST_Contains(nng.geometry, ST_SetSRID(l.position, 4326));
-
-        SELECT nng.borough into default_borough from
-          locations l
-          cross join nyc_neighborhood_geometries  nng
-          where l.id = NEW.location_id 
-            and ST_Contains(nng.geometry, ST_SetSRID(l.position, 4326));
-
-        SELECT nd.district_id into default_school_district from
-          locations l
-          cross join nyc_districts nd
-          where l.id = NEW.location_id 
-            and ST_Contains(nd.geometry, ST_SetSRID(l.position, 4326))
-            and nd.type = 'school';
-
-        SELECT nd.district_id into default_congressional_district from
-          locations l
-          cross join nyc_districts nd
-          where l.id = NEW.location_id 
-            and ST_Contains(nd.geometry, ST_SetSRID(l.position, 4326))
-            and nd.type = 'congressional';
-
-        SELECT nd.district_id into default_community_district from
-          locations l
-          cross join nyc_districts nd
-          where l.id = NEW.location_id 
-            and ST_Contains(nd.geometry, ST_SetSRID(l.position, 4326))
-            and nd.type = 'community';
-
-        NEW.neighborhood := default_neighborhood;
-        NEW.borough := default_borough;
-        NEW.school_district = default_school_district;
-        NEW.congressional_district = default_congressional_district;
-        NEW.community_district = default_community_district;
-        return NEW;
-      end;
-      $$;
-    `);
+      DROP TRIGGER IF EXISTS set_default_neighborhood_on_physical_address_update_trigger 
+        ON physical_addresses`);
+    await queryInterface.sequelize.query(`
+      DROP FUNCTION IF EXISTS handle_set_default_neighborhood_on_physical_address_update_trigger`);
+    // drop the neighborhood field
+    await queryInterface.removeColumn('physical_addresses', 'neighborhood');
+    // TODO: create a view that queries the neighborhood, district, etc.
+    // primary key based on the location id. so that we can join to this view.
   },
 
   async down(queryInterface, Sequelize) {
-    return queryInterface.sequelize.transaction(async t => Promise.all([
-      queryInterface.removeColumn('physical_addresses', 'borough', { transaction: t }),
-      queryInterface.removeColumn('physical_addresses', 'school_district', { transaction: t }),
-      queryInterface.removeColumn(
-        'physical_addresses',
-        'congressional_district',
-        { transaction: t },
-      ),
-      queryInterface.removeColumn('physical_addresses', 'community_district', { transaction: t }),
-    ]));
+    // create the neighborhood field
+    await (!isTesting ?
+      queryInterface.addColumn('physical_addresses', 'neighborhood', {
+        type: Sequelize.DataTypes.STRING,
+      }) :
+      new Promise(resolve => resolve()));
+    // create the triggers
+    await queryInterface.sequelize.query(`
+          create or replace function 
+            handle_set_default_neighborhood_on_physical_address_insert_trigger()
+             returns trigger
+             language plpgsql
+            as
+          $$
+          DECLARE 
+            default_neighborhood varchar;
+          begin
+
+            SELECT neighborhood into default_neighborhood from 
+              nyc_neighborhoods where 
+                nyc_neighborhoods.zip_code = NEW.postal_code;
+
+            NEW.neighborhood := default_neighborhood;
+            return NEW;
+          end;
+          $$;
+
+        CREATE TRIGGER set_default_neighborhood_on_physical_address_insert_trigger 
+           BEFORE insert
+           ON physical_addresses
+           FOR EACH ROW
+               EXECUTE PROCEDURE 
+                 handle_set_default_neighborhood_on_physical_address_insert_trigger();
+
+
+        create or replace function 
+            handle_set_default_neighborhood_on_physical_address_update_trigger()
+             returns trigger
+             language plpgsql
+            as
+          $$
+          DECLARE 
+            default_neighborhood varchar;
+          begin
+
+            -- if we change his postal code, then reset his neighborhood to the default
+            if NEW.postal_code <> OLD.postal_code and NEW.postal_code is not null then
+              SELECT neighborhood into default_neighborhood from 
+                nyc_neighborhoods where 
+                  nyc_neighborhoods.zip_code = NEW.postal_code;
+
+              UPDATE physical_addresses set neighborhood = default_neighborhood
+                WHERE id = NEW.id;
+            end if;
+            return NEW;
+          end;
+          $$;
+
+        CREATE TRIGGER set_default_neighborhood_on_physical_address_update_trigger 
+           AFTER update
+           ON physical_addresses
+           FOR EACH ROW
+               EXECUTE PROCEDURE 
+                 handle_set_default_neighborhood_on_physical_address_update_trigger();
+
+      `);
   },
 };
