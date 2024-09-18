af9bef334b56ab0bc05d1272d848f282a1c08e20
Jacob Beard <jake@minnow.io>
2024-09-18 06:56:43 -0600

    Add the neighborhood subquery to getInfoBySlug

--- src/controllers/locations.js
+++ src/controllers/locations.js
@@ -62,6 +62,24 @@ const getInfoAssociations = {
   ],
 };
 
+const getNeighborhoodAttributeSubquery = {
+  attributes: {
+    include: [
+      [
+        models.sequelize.literal(`(
+            SELECT neighborhood
+            FROM nyc_neighborhood_geometries
+            WHERE ST_Contains(
+              nyc_neighborhood_geometries.geometry, 
+              ST_SetSRID(position,4326)
+            )
+        )`),
+        'neighborhood',
+      ],
+    ],
+  },
+}
+
 async function handleGetInfoResponse(location, excludeMetadata) {
   if (!location) {
     throw new NotFoundError('Location not found');
@@ -263,21 +281,7 @@ export default {
         req.params.locationId,
         {
           include: getInfoAssociations.include,
-          attributes: {
-            include: [
-              [
-                models.sequelize.literal(`(
-                    SELECT neighborhood
-                    FROM nyc_neighborhood_geometries
-                    WHERE ST_Contains(
-                      nyc_neighborhood_geometries.geometry, 
-                      ST_SetSRID(position,4326)
-                    )
-                )`),
-                'neighborhood',
-              ],
-            ],
-          },
+          attributes: getNeighborhoodAttributeSubquery.attributes,
         },
       );
 
@@ -297,6 +301,7 @@ export default {
           slug: req.params.slug,
         },
         include: getInfoAssociations.include,
+        attributes: getNeighborhoodAttributeSubquery.attributes,
       });
 
       if (!locations.length) {
