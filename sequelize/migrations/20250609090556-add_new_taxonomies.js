2e9e5004f28b8d1c1c021b965e1fff57fb3b29e3
shakil hossain <sh9743441@gmail.com>
2025-07-23 15:45:22 +0600

    create new taxonomies with createInstance funtion and destroyInstance for delete

--- sequelize/migrations/20250609090556-add_new_taxonomies.js
+++ sequelize/migrations/20250609090556-add_new_taxonomies.js
@@ -1,69 +1,52 @@
-import uuid from 'uuid/v4';
+import models from '../../src/models';
+import { createInstance, destroyInstance } from '../../src/services/data-changes';
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     const taxonomies = [
       {
-        id: uuid(), name: 'Employment', parent_name: 'Other service', created_at: new Date(), updated_at: new Date(), parent_id: '1fad2545-15d0-4613-b34c-1407b8b7e74e',
+        name: 'Employment',
+        parent_name: 'Other service',
+        parent_id: '1fad2545-15d0-4613-b34c-1407b8b7e74e',
       },
       {
-        id: uuid(), name: 'Mental Health', parent_name: 'Health', created_at: new Date(), updated_at: new Date(), parent_id: 'e838dbf0-21f8-47c2-b17d-57543e8dbffb',
+        name: 'Mental Health',
+        parent_name: 'Health',
+        parent_id: 'e838dbf0-21f8-47c2-b17d-57543e8dbffb',
       },
       {
-        id: uuid(), name: 'Legal Services', parent_name: 'Other service', created_at: new Date(), updated_at: new Date(), parent_id: '1fad2545-15d0-4613-b34c-1407b8b7e74e',
+        name: 'Legal Services',
+        parent_name: 'Other service',
+        parent_id: '1fad2545-15d0-4613-b34c-1407b8b7e74e',
       },
     ];
 
-    const metadata = taxonomies.flatMap((record => ([
-      {
-        id: uuid(),
-        resource_id: record.id,
-        resource_table: 'taxonomies',
-        last_action_date: new Date(),
-        last_action_type: 'create',
-        field_name: 'name',
-        replacement_value: record.name,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        id: uuid(),
-        resource_id: record.id,
-        resource_table: 'taxonomies',
-        last_action_date: new Date(),
-        last_action_type: 'create',
-        field_name: 'parent_name',
-        replacement_value: record.parent_name,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-      {
-        id: uuid(),
-        resource_id: record.id,
-        resource_table: 'taxonomies',
-        last_action_date: new Date(),
-        last_action_type: 'create',
-        field_name: 'parent_id',
-        replacement_value: record.parent_id,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]
-    )));
+    const modelCreateFunction = models.Taxonomy.create.bind(models.Taxonomy);
 
-    await queryInterface.bulkInsert('taxonomies', taxonomies);
-    queryInterface.bulkInsert('metadata', metadata);
+    for (const taxonomy of taxonomies) {
+      await createInstance('<System>', modelCreateFunction, taxonomy, {
+        metadata: {
+          source: 'migration',
+        },
+      });
+    }
   },
 
   async down(queryInterface, Sequelize) {
     const taxonomyNames = ['Employment', 'Mental Health', 'Legal Services'];
 
-    await queryInterface.bulkDelete('taxonomies', {
-      name: { [Sequelize.Op.in]: taxonomyNames },
+    const taxonomies = await models.Taxonomy.findAll({
+      where: {
+        name: taxonomyNames,
+      },
     });
-    await queryInterface.bulkDelete('metadata', {
-      resource_table: 'taxonomies',
-      replacement_value: { [Sequelize.Op.in]: taxonomyNames },
-    });
+
+    for (const taxonomy of taxonomies) {
+      await destroyInstance('<System>', taxonomy, {
+        metadata: {
+          source: 'migration',
+        },
+      });
+    }
   },
 };
