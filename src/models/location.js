043b9b34737c2a03b2e389efe24d2d7dbe2a05d9
Shakil Hossain <82315234+shakilhossain1@users.noreply.github.com>
2025-09-08 18:46:05 +0600

    Fix location slug redirects (#151)

--- src/models/location.js
+++ src/models/location.js
@@ -69,6 +69,7 @@ module.exports = (sequelize, DataTypes, Op) => {
     Location.hasMany(models.EventRelatedInfo, { foreignKey: 'location_id' });
     Location.hasMany(models.Comment, { foreignKey: 'location_id' });
     Location.hasMany(models.ErrorReport, { foreignKey: 'location_id' });
+    Location.hasMany(models.LocationSlugRedirect, { foreignKey: 'location_id' });
 
     // Can't just set defaultScope on the initial model definition:
     // https://github.com/sequelize/sequelize/issues/6245.
