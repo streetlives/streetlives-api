e735bd4c0e87fe77fd1f11493c90da17d0e60af9
Micksterious <Michael.r.javier@gmail.com>
2026-03-30 22:54:39 +0000

    Updated test.yml

--- scripts/test-db.js
+++ scripts/test-db.js
@@ -4,11 +4,11 @@ const { Client } = require('pg');
 
 async function testDB() {
   const client = new Client({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
   });
 
   try {
