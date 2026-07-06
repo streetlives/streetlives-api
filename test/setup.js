329959e16aaf9aec37d5d40a545c48f39e0ac42f
Jacob Beard <jake@minnow.io>
2026-07-06 06:36:24 -0600

    Add GitHub Actions CI and stabilize test database setup (#210)
    Add GitHub Actions test workflow

--- test/setup.js
+++ test/setup.js
@@ -3,9 +3,7 @@ const exec = util.promisify(require('child_process').exec);
 
 jest.setTimeout(10000);
 
-process.env.DATABASE_NAME = 'test';
-process.env.DATABASE_LOGGING = 'false';
-process.env.OPENAI_API_KEY = process.env.OPENAI_API_KEY || 'test-key-placeholder';
+require('./env');
 
 const models = require('../src/models');
 
@@ -37,6 +35,7 @@ beforeAll(async () => {
         p.tablename = t.table_name and p.schemaname = t.table_schema
         where table_schema = 'public' and 
           table_type='BASE TABLE' and p.tableowner = 'streetlives'
+          and t.table_name != 'spatial_ref_sys'
       LOOP
         EXECUTE format('drop table %I cascade',_table.table_name);
       END LOOP;
@@ -45,6 +44,12 @@ beforeAll(async () => {
   // for (let [table] of tables){
   //  await models.sequelize.query(`drop table ${table} cascade`);
   // }
+
+  await models.sequelize.query('DROP TYPE IF EXISTS age_eligibility CASCADE');
+
+  await models.sequelize.query('CREATE EXTENSION IF NOT EXISTS postgis');
+
+  await models.sequelize.query('CREATE EXTENSION IF NOT EXISTS fuzzystrmatch');
 
   await models.sequelize.sync({ force: true });
 
@@ -75,7 +80,5 @@ beforeAll(async () => {
   await execScript('npx sequelize-cli db:migrate --name 20240607172205-age-filter');
 });
 afterAll(async () => {
-  // eslint-disable-next-line no-implied-eval
-  await execScript('npx sequelize-cli db:migrate:undo --name 20240607172205-age-filter');
   await models.sequelize.close();
 });
